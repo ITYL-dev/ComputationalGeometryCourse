65c2f5d1ed0a8793dcb5576515fe1329b2661179
Baptiste Grignon <51024276+ITYL-dev@users.noreply.github.com>
2025-02-11 11:18:58 +0100

    clean code 2

--- TPs/TPs.cpp
+++ TPs/TPs.cpp
@@ -1,19 +1,12 @@
 #include <cmath>
-#include <cstdlib>
 #include <fstream>
 #include <initializer_list>
 #include <iostream>
 #include <map>
 #include <set>
 #include <sstream>
-#include <stdexcept>
 #include <string>
-#include <utility>
 #include <vector>
-
-typedef unsigned long long size_t; // should already be defined in cstdib,
-// but I redefine it in case size_t is not unsigned long long (even it's very unlikely),
-// to ensure compatibility with std::stoull (see Mesh::readOFF)
 
 double sqr(double a) { return a * a; }
 
@@ -85,42 +78,36 @@ Vector3 cross(const Vector3& a, const Vector3& b) {
 class Vertex3 {
 public:
 
-    Vertex3(std::initializer_list<double> position, size_t index_adjacent_triangle, bool is_virtual = false) :
+    Vertex3(std::initializer_list<double> position, int index_adjacent_triangle, bool is_virtual = false) :
         index_adjacent_triangle(index_adjacent_triangle), is_virtual(is_virtual) { updatePosition(position); }
 
     void updatePosition(std::initializer_list<double> position) {
-
-        if (position.size() != 3) throw std::invalid_argument("Invalid initializer list : 3 values are expected");
-        for (size_t i{ 0 }; i < 3; i++) this->position[i] = position.begin()[i];
+        for (int i{ 0 }; i < 3; i++) this->position[i] = position.begin()[i];
     }
 
     Vector3 position;
-    size_t index_adjacent_triangle;
+    int index_adjacent_triangle;
     bool is_virtual;
 };
 
 class Triangle {
 public:
 
-    Triangle(std::initializer_list<size_t> vertices_indices, std::initializer_list<size_t> neighbouring_triangles_indices) {
+    Triangle(std::initializer_list<int> vertices_indices, std::initializer_list<int> neighbouring_triangles_indices) {
         updateVertices(vertices_indices);
         updateNT(neighbouring_triangles_indices);
     }
 
-    void updateVertices(std::initializer_list<size_t> vertices_indices) {
-
-        if (vertices_indices.size() != 3) throw std::invalid_argument("Invalid initializer list : 3 values are expected");
-        for (size_t i{ 0 }; i < 3; i++) this->vertices_indices[i] = vertices_indices.begin()[i];
+    void updateVertices(std::initializer_list<int> vertices_indices) {
+        for (int i{ 0 }; i < 3; i++) this->vertices_indices[i] = vertices_indices.begin()[i];
     }
 
-    void updateNT(std::initializer_list<size_t> neighbouring_triangles_indices) {
-
-        if (neighbouring_triangles_indices.size() != 3) throw std::invalid_argument("Invalid initializer list : 3 values are expected");
-        for (size_t i{ 0 }; i < 3; i++) this->neighbouring_triangles_indices[i] = neighbouring_triangles_indices.begin()[i];
+    void updateNT(std::initializer_list<int> neighbouring_triangles_indices) {
+        for (int i{ 0 }; i < 3; i++) this->neighbouring_triangles_indices[i] = neighbouring_triangles_indices.begin()[i];
     }
 
-    size_t vertices_indices[3];
-    size_t neighbouring_triangles_indices[3];
+    int vertices_indices[3];
+    int neighbouring_triangles_indices[3];
     bool is_virtual{ false };
 };
 
@@ -149,9 +136,9 @@ public:
         ofs << "OFF\n";
         ofs << vertices.size() - nb_virtual_vertices << " " << triangles.size() - nb_virtual_triangles << " 0\n";
 
-        for (size_t i{ 0 }; i < vertices.size(); i++) {
+        for (int i{ 0 }; i < vertices.size(); i++) {
             if (!vertices[i].is_virtual) {
-                for (size_t j{ 0 }; j < 3; j++) {
+                for (int j{ 0 }; j < 3; j++) {
                     ofs << vertices[i].position[j];
                     if (j < 2) ofs << " ";
                 }
@@ -159,10 +146,10 @@ public:
             }
         }
 
-        for (size_t i{ 0 }; i < triangles.size(); i++) {
+        for (int i{ 0 }; i < triangles.size(); i++) {
             if (!triangles[i].is_virtual) {
                 ofs << "3";
-                for (size_t j{ 0 }; j < 3; j++) {
+                for (int j{ 0 }; j < 3; j++) {
                     ofs << " " << triangles[i].vertices_indices[j];
                 }
                 ofs << "\n";
@@ -182,9 +169,9 @@ public:
             exit(1);
         }
 
-        std::map<std::set<size_t>, std::pair<size_t, size_t>> map;
+        std::map<std::set<int>, std::pair<int, int>> map;
 
-        size_t nb_vertices{ 0 }, nb_triangles{ 0 }, line_counter{ 0 }, bound1{ 2 }, bound2{ 0 }, bound3{ 0 };
+        int nb_vertices{ 0 }, nb_triangles{ 0 }, line_counter{ 0 }, bound1{ 2 }, bound2{ 0 }, bound3{ 0 };
         std::string line;
         while (std::getline(ifs, line)) { // parsing using order (comments not supported)
                 
@@ -192,50 +179,48 @@ public:
 
             if (line_counter == 1) { // 2nd line (assuming there is a first line with OFF)
 
-                nb_vertices = std::stoull(v_line[0]);
+                nb_vertices = std::stoi(v_line[0]);
                 bound2 = bound1 + nb_vertices;
-                for (size_t i{ 0 }; i < nb_vertices; i++) addVertex(Vertex3({ 0, 0, 0 }, 0));
+                for (int i{ 0 }; i < nb_vertices; i++) addVertex(Vertex3({ 0, 0, 0 }, 0));
 
-                nb_triangles = std::stoull(v_line[1]);
+                nb_triangles = std::stoi(v_line[1]);
                 bound3 = bound2 + nb_triangles;
-                for (size_t i{ 0 }; i < nb_triangles; i++) addTriangle(Triangle({ 0, 0, 0 }, { 0, 0, 0 }));
+                for (int i{ 0 }; i < nb_triangles; i++) addTriangle(Triangle({ 0, 0, 0 }, { 0, 0, 0 }));
                 // default neighbouring triangle will be the first one in case the mesh is not closed
             }
             else if (bound1 <= line_counter && line_counter < bound2) {
 
-                size_t i{ line_counter - bound1 };
+                int i{ line_counter - bound1 };
                 vertices[i].updatePosition({ std::stod(v_line[0]), std::stod(v_line[1]), std::stod(v_line[2]) });
             }
             else if (bound2 <= line_counter && line_counter < bound3) {
 
-                size_t i{ line_counter - bound2 };
+                int i{ line_counter - bound2 };
 
                 // we trust the file that the vertices indices are in the right (trigonometric) order
                 // only support triangles reading
-                triangles[i].vertices_indices[0] = std::stoull(v_line[1]);
-                triangles[i].vertices_indices[1] = std::stoull(v_line[2]);
-                triangles[i].vertices_indices[2] = std::stoull(v_line[3]);
+                triangles[i].updateVertices({ std::stoi(v_line[1]), std::stoi(v_line[2]), std::stoi(v_line[3])});
                 // adjacent triangle for a Vertex3 will be the last adjacent triangle "seen"
                 vertices[triangles[i].vertices_indices[0]].index_adjacent_triangle = i;
                 vertices[triangles[i].vertices_indices[1]].index_adjacent_triangle = i;
                 vertices[triangles[i].vertices_indices[2]].index_adjacent_triangle = i;
 
-                std::vector<std::set<size_t>> edges;
+                std::vector<std::set<int>> edges;
                 edges.reserve(3);
-                edges.emplace_back(std::set<size_t>{ triangles[i].vertices_indices[1], triangles[i].vertices_indices[2] }); // relative index 0
-                edges.emplace_back(std::set<size_t>{ triangles[i].vertices_indices[2], triangles[i].vertices_indices[0] }); // relative index 1
-                edges.emplace_back(std::set<size_t>{ triangles[i].vertices_indices[0], triangles[i].vertices_indices[1] }); // relative index 2
+                edges.emplace_back(std::set<int>{ triangles[i].vertices_indices[1], triangles[i].vertices_indices[2] }); // relative index 0
+                edges.emplace_back(std::set<int>{ triangles[i].vertices_indices[2], triangles[i].vertices_indices[0] }); // relative index 1
+                edges.emplace_back(std::set<int>{ triangles[i].vertices_indices[0], triangles[i].vertices_indices[1] }); // relative index 2
                 // relative index k = index (inside of the Triangle class) of a Vertex3, the edge opposite to the Vertex3 and the triangle connected to opposite edge
 
-                for (size_t k{ 0 }; k < 3; k++) {
+                for (int k{ 0 }; k < 3; k++) {
                     if (map.count(edges[k])) { // 1 ...
 
-                        size_t i_bis{ map[edges[k]].first };
+                        int i_bis{ map[edges[k]].first };
                         triangles[i].neighbouring_triangles_indices[k] = i_bis;
                         triangles[i_bis].neighbouring_triangles_indices[map[edges[k]].second] = i;
                     }
                     else { // ... or 0
-                        map[edges[0]] = std::pair<size_t, size_t>(i, k);
+                        map[edges[0]] = std::pair<int, int>(i, k);
                     }
                 }
             }
@@ -250,16 +235,16 @@ public:
     void check_virtual() {
 
         nb_virtual_vertices = 0;
-        for (size_t i{ 0 }; i < vertices.size(); i++)
+        for (int i{ 0 }; i < vertices.size(); i++)
             if (vertices[i].is_virtual)
                 nb_virtual_vertices += 1;
 
         nb_virtual_triangles = 0;
-        for (size_t i{ 0 }; i < triangles.size(); i++) {
+        for (int i{ 0 }; i < triangles.size(); i++) {
 
             bool triangle_is_virtual{ false };
 
-            for (size_t j{ 0 }; j < 3; j++)
+            for (int j{ 0 }; j < 3; j++)
                 if (vertices[triangles[i].vertices_indices[j]].is_virtual)
                     triangle_is_virtual = true;
 
@@ -268,8 +253,8 @@ public:
         }
     }
 
-    size_t nb_virtual_vertices{ 0 };
-    size_t nb_virtual_triangles{ 0 };
+    int nb_virtual_vertices{ 0 };
+    int nb_virtual_triangles{ 0 };
 
     std::vector<Vertex3> vertices;
     std::vector<Triangle> triangles;
